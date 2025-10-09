beaa1eb43533528f116b67472711b787bcac354d
eight04 <eight04@gmail.com>
2025-10-09 17:00:24 +0800

    Add: a new way to call fetch from page script

--- src/content.mjs
+++ src/content.mjs
@@ -1,14 +1,26 @@
 import browser from "webextension-polyfill";
 import {xhr, fetchBlob} from "./xhr.mjs";
 
+function callEval(f, ...args) {
+  return window.eval(`(${f.toString()})(${args.map(a => JSON.stringify(a)).join(',')})`);
+}
+
 const METHODS = {
-  fetch({url, mode, page}) {
-    return fetchBlob(url, {mode, page});
-    // const _fetch = page ? content.fetch : fetch;
-    // return _fetch(url, {mode});
+  fetch({url, ...args}) {
+    if (args.eval && args.page) {
+      delete args.eval;
+      delete args.page;
+      return callEval(fetchBlob, url, args);
+    }
+    return fetchBlob(url, args);
   },
-  xhr({url, page}) {
-    return xhr(url, page);
+  xhr(args) {
+    if (args.eval && args.page) {
+      delete args.eval;
+      delete args.page;
+      return callEval(xhr, args);
+    }
+    return xhr(args);
   },
   async checkPageLoad() {
     if (document.readyState === "complete") {
